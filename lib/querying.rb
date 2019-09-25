a8102d810c914e2bc33c24001ae669058525bcaf
Pablo Gonzalez <pabgon0@gmail.com>
2019-09-25 23:16:00 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -30,7 +30,7 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "SELECT series.title
+  "SELECT title
   FROM series
   JOIN characters ON characters.author_id = series.author_id
   GROUP BY species
