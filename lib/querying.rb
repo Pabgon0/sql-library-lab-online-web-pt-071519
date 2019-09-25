ac5f62434b7c6c46754d7437b5bbbeda2de87871
Pablo Gonzalez <pabgon0@gmail.com>
2019-09-25 23:15:42 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -34,7 +34,8 @@ def select_series_title_with_most_human_characters
   FROM series
   JOIN characters ON characters.author_id = series.author_id
   GROUP BY species
-  ORDER BY COUNT(species)"
+  ORDER BY COUNT(species)
+  LIMIT 1"
 end
 
 def select_character_names_and_number_of_books_they_are_in
